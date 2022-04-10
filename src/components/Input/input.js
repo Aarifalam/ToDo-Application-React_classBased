410a2483848ba8cbf57fa33aafb5643b7d94cea1
aarifalam <aarif20@navgurukul.org>
2022-04-10 11:51:43 +0530

    in previous i forgot to give state-value at app.js in constructor.

--- src/components/Input/input.js
+++ src/components/Input/input.js
@@ -1,21 +1,10 @@
 import React from "react";
 
 class Input extends React.Component {
-
-    // these commented lines will remove
-    // constructor(props) {
-    //     super(props);
-    //     this.handleChange = this.handleChange.bind(this);
-    // }
-    // handleChange(e) {
-    //     this.props.updatetodos({ value: e.target.value })
-    // }
-
     render() {
         return (
             <div>
                 <input ref={this.props.inputRef} />
-                {/* <input onChange={this.handleChange} /> */}
             </div>
         );
     };
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -6,48 +6,29 @@ import List from "../../components/List";
 import Input from "../../components/Input/input";
 
 
-// whenever we update the state it will update the UI
-// just to get the value of the input tag do we need to update all these conponents, it is not good prectice
-
-// we don't use input tags or much more because we don't want to update the components by our self. that's why we use React. React takes care for it  because react has middle layer which called vertual dom.
-
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            // value: "",
+            value: "",
             todos: []
         }
         this.updatetodos = this.updatetodos.bind(this)
         this.updateList = this.updateList.bind(this)
         this.deleteTodos = this.deleteTodos.bind(this)
-        // this.input = React.createRef()     //this way can get createRef()
-        // this.input = createRef()
     }
 
     updatetodos(newTodos) {
         this.setState(newTodos);
     };
 
-    // for to focus any thing after we do refress .focus in  componentDiMount function
     componentDidMount() {
         this.input.focus()
-
-        // this.input.current.focus()
     }
 
     updateList() {
-        // console.log(this.input)    // this is object which ha current key and current has input 
-        // console.log(this.input.current)    // to get the value of this input
-        // console.log(this.input.value)
-        // this is exect same as documen.id selector or querry or class. we used this ref because this will not update all component
-        // in this case we not updating state or UI
-
         var copyTodos = [...this.state.todos]
-        // console.log(copyTodos)
         copyTodos.push(this.input.value)
-        // copyTodos.push(this.input.current.value)
-        // copyTodos.push(this.state.value)
         this.updatetodos({
             todos: copyTodos
         });
@@ -66,14 +47,7 @@ class App extends React.Component {
             <div id="App" className="flex">
                 <Title title="Todo Application" />
                 <div style={{ display: "flex", border: "1px solid blue" }}>
-                    {/* if i do not want to change the reference input */}
                     <Input inputRef={el => this.input = el} />
-                    {/* this is prop forwading, when this input will call this.input will become el */}
-
-                    {/* this is for direct input which will not  */}
-                    {/* <input ref={this.input} /> */}
-
-                    {/* <Input todos={this.state.value} updatetodos={this.updatetodos} deleteTodos={this.deleteTodos} /> */}
                     <button onClick={this.updateList}>Click me</button>
                 </div>
                 <List todos={this.state.todos}
@@ -84,5 +58,3 @@ class App extends React.Component {
     }
 }
 export default App;
-
-// when we were updaing the value it was by default refresh the other component which reaquire the  value
