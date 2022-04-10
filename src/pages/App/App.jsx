52a9d6533af0268c7c5dd49c3acfe288eeff9937
aarifalam <aarif20@navgurukul.org>
2022-04-10 13:05:51 +0530

    1 -> because of State-Value it is refreshing whole dom but using of Ref we can simply use html input tag and it willn't refresh the dom. 2->used componentDidMount for to focus on input bydefault.

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,34 +1,49 @@
-import React from "react";
+import React, { createRef } from "react";
 import "./app.css"
 // import Input from "../../components/Input/input";
 import Title from "../../components/Title/title"
 import List from "../../components/List";
-import Input from "../../components/Input/input";
 
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: "",
+            // value: "",
             todos: []
         }
         this.updatetodos = this.updatetodos.bind(this)
         this.updateList = this.updateList.bind(this)
         this.deleteTodos = this.deleteTodos.bind(this)
+
+        //3 first we need to create veriable of Ref 
+        // this.input = React.createRef()  // other way in below to write this
+        this.input = createRef()  //export default way
+        // this creating reference is simply creating a selector, you are directly getting html element 
     }
+    //1 just to get the input value do we need to update all components, this is not a good practice.
+    // because whenever i am typing over input the app should not get updated because it will update all componets which contains by app
+
+    componentDidMount() {
+        this.input.current.focus()
+    }
+    // beacuse of this by default it will focus on input 
 
     updatetodos(newTodos) {
         this.setState(newTodos);
     };
 
-    componentDidMount() {
-        this.input.focus()
-    }
+    // componentDidMount() {
+    //     this.input.focus()
+    // }
 
     updateList() {
+        // console.log(this.input.current.value)
         var copyTodos = [...this.state.todos]
-        copyTodos.push(this.input.value)
+        // copyTodos.push(this.input.value)
+        copyTodos.push(this.input.current.value)
+        // this value we are getting from the input html tag 
+
         this.updatetodos({
             todos: copyTodos
         });
@@ -47,7 +62,17 @@ class App extends React.Component {
             <div id="App" className="flex">
                 <Title title="Todo Application" />
                 <div style={{ display: "flex", border: "1px solid blue" }}>
-                    <Input inputRef={el => this.input = el} />
+
+                    {/* <input id="in" /> */}
+                    {/*2 we can get input by id or querry selector. it is not recummended to use this.
+                    we use React to not update Dom by ourself using document.getElementById or querry.
+                    React takes care of it because there is a middle-layer called vertual dom.  */}
+                    {/* if you are insistedly giving by document.getElementByid or querry at some place where this input is still not there in the UI  */}
+                    {/* to handle this React has something Ref */}
+                    <input ref={this.input} />
+                    {/* 4 go to  update list do console.log this will give value and this we are getting from direct input and this is not updating the Dom */}
+
+                    {/* <Input inputRef={el => this.input = el} /> */}
                     <button onClick={this.updateList}>Click me</button>
                 </div>
                 <List todos={this.state.todos}
